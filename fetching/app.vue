c2d1e8dc341f71e6df8a87e7a59f3cf6c6729c6b
Ana Nuno <nunoany@gmail.com>
2024-08-02 10:48:09 -0500

    First commit of the year, I'll improve this repo!

--- fetching/app.vue
+++ fetching/app.vue
@@ -1,17 +1,21 @@
 <script setup lang="ts">
+// Can use this for date api
+// https://timeapi.io/swagger/index.html
 //https://api.openweathermap.org/data/2.5/weather?q=Toronto&appid=64e28f64b16b0f6cfeea0039ad3536da
 
-const search = ref("Toronto");
+const search = ref("Guadalajara");
 const input = ref("");
 const background = ref("");
 
-
 //adding () => will refresh the fetching process
-const {data: city, error} = useFetch(() => `https://api.openweathermap.org/data/2.5/weather?q=${search.value}&units=metric&appid=64e28f64b16b0f6cfeea0039ad3536da`);
+const { data: city, error } = useFetch(
+  () =>
+    `https://api.openweathermap.org/data/2.5/weather?q=${search.value}&units=metric&appid=64e28f64b16b0f6cfeea0039ad3536da`
+);
 
 // const {data: city, error } = useAsyncData("city", async () => {
 //   const response = await $fetch(`https://api.openweathermap.org/data/2.5/weather?q=${search.value}&units=metric&appid=64e28f64b16b0f6cfeea0039ad3536da`);
-  
+
 //   const temp = response.main.temp;
 
 //   if (temp <= -10) {
@@ -27,7 +31,7 @@ const {data: city, error} = useFetch(() => `https://api.openweathermap.org/data/
 //         background.value =
 //           "https://images.unsplash.com/photo-1507525428034-b723cf961d3e?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=3546&q=80";
 //       }
-    
+
 //   return response;
 // }
 
@@ -35,48 +39,55 @@ const {data: city, error} = useFetch(() => `https://api.openweathermap.org/data/
 
 const handleClick = () => {
   // Los Angeles => Los+Angeles
-  const formatedSearch = input.value.trim().split(" ").join("+")
+  const formatedSearch = input.value.trim().split(" ").join("+");
   search.value = formatedSearch;
-  input.value =  "";
-}
-
-
+  input.value = "";
+};
 </script>
-
 
 <template>
   <div>
     <div class="h-screen relative overflow-hidden">
-      <img :src="background"/>
-      <div class="absolute w-full h-full top-0 overlay"/>
+      <img :src="background" />
+      <div class="absolute w-full h-full top-0 overlay" />
       <div class="absolute w-full h-full top-0 p-48">
-
         <div class="flex justify-between">
           <div>
-            <h1 class="text-7xl text-white">{{city.name}}</h1>
-            <p class="font-extralight text-2xl mt-2 text-white">Sunday Dec 4th</p>
-            <img :src="`https://openweathermap.org/img/wn/${city.weather[0].icon}@4x.png`" class="w-56 icon" />
+            <h1 class="text-7xl text-white">{{ city.name }}</h1>
+            <p class="font-extralight text-2xl mt-2 text-white">
+              Sunday Dec 4th
+            </p>
+            <img
+              :src="`https://openweathermap.org/img/wn/${city.weather[0].icon}@4x.png`"
+              class="w-56 icon"
+            />
           </div>
           <div>
             <p class="text-9xl text-white font-extralight">
-              {{city.main.temp}}°
+              {{ city.main.temp }}°
             </p>
           </div>
         </div>
 
         <div class="mt-20">
-          <input type="text" class="w-1/2 h-10 px-4" placeholder="Search a city" v-model="input"/>
-          <button class="bg-cyan-500 w-20 text-white h-10" @click="handleClick">Search</button>
+          <input
+            type="text"
+            class="w-1/2 h-10 px-4"
+            placeholder="Search a city"
+            v-model="input"
+          />
+          <button class="bg-cyan-500 w-20 text-white h-10" @click="handleClick">
+            Search
+          </button>
         </div>
-
       </div>
     </div>
   </div>
 </template>
 
 <style scoped>
-.overlay  {
-  background-color: #AAA196;
+.overlay {
+  background-color: #aaa196;
 }
 
 /* .icon{
