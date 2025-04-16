37d752100ebf463e8a9cf3bf835fe6d20411b2be
Ana Nuno <nunoany@gmail.com>
2025-04-15 23:22:22 -0500

    trabajando en cambiar el layout y colores

--- fetching/app.vue
+++ fetching/app.vue
@@ -3,7 +3,7 @@
 // https://timeapi.io/swagger/index.html
 //https://api.openweathermap.org/data/2.5/weather?q=Toronto&appid=64e28f64b16b0f6cfeea0039ad3536da
 
-const search = ref("Guadalajara");
+const search = ref("San Francisco");
 const input = ref("");
 const background = ref("");
 
@@ -47,26 +47,31 @@ const handleClick = () => {
 
 <template>
   <div>
-    <div class="relative h-screen w-screen overlay mx-auto">
+    <div class="relative w-screen h-screen mx-auto bg-primaryWhite">
       <div class="flex flex-col justify-center pt-20 md:pt-60">
-        <h1 class="flex justify-center text-white text-7xl">{{ city.name }}</h1>
+        <h1 class="flex justify-center text-primaryBlue text-7xl">
+          {{ city.name }}
+        </h1>
         <div class="flex justify-center mt-16 md:mt-20">
           <input
             type="text"
-            class="w-1/2 h-10 px-4"
+            class="w-1/2 h-12 px-6 text-white placeholder rounded-l-3xl bg-primaryBeige"
             placeholder="Search a city"
             v-model="input"
           />
-          <button class="bg-cyan-500 w-20 text-white h-10" @click="handleClick">
+          <button
+            class="h-12 px-12 text-white bg-primaryBlue rounded-r-3xl"
+            @click="handleClick"
+          >
             Search
           </button>
         </div>
       </div>
       <div
-        class="bg-slate overflow-auto absolute inset-x-0 bottom-0 w-11/12 h-4/6 md:h-1/2 rounded-t-2xl mx-auto"
+        class="absolute inset-x-0 bottom-0 w-11/12 mx-auto overflow-auto bg-primaryGreen h-4/6 md:h-1/2 rounded-t-2xl"
       >
-        <div class="flex flex-col xl:flex-row gap-5 p-10 something">
-          <div class="h-2/5 xl:h-full xl:w-2/5 bg-mist rounded-xl p-10">
+        <div class="flex flex-col gap-5 p-10 xl:flex-row something">
+          <div class="p-10 h-2/5 xl:h-full xl:w-2/5 bg-primaryWhite rounded-xl">
             <div class="flex justify-center text-3xl">{{ city.main.temp }}</div>
             <!-- <div class="flex justify-center">
               <img
@@ -76,26 +81,28 @@ const handleClick = () => {
             </div> -->
           </div>
           <div class="flex flex-col gap-5 h-3/5 xl:h-full xl:w-3/5">
-            <div class="flex flex-col md:flex-row gap-5 h-1/2">
-              <div class="md:w-1/3 bg-mist rounded-xl text-keylime p-10">
+            <div class="flex flex-col gap-5 md:flex-row h-1/2">
+              <div
+                class="p-10 md:w-1/3 bg-primaryWhite rounded-xl text-keylime"
+              >
                 Feels like
                 {{ city.main.feels_like }}
               </div>
-              <div class="md:w-1/3 bg-mist rounded-xl p-10">
+              <div class="p-10 md:w-1/3 bg-primaryWhite rounded-xl">
                 Temp Min {{ city.main.temp_min }}
               </div>
-              <div class="md:w-1/3 bg-mist rounded-xl p-10">
+              <div class="p-10 md:w-1/3 bg-primaryWhite rounded-xl">
                 Temp Max {{ city.main.temp_max }}
               </div>
             </div>
-            <div class="flex flex-col md:flex-row gap-5 h-1/2">
-              <div class="md:w-1/3 bg-mist rounded-xl p-10">
+            <div class="flex flex-col gap-5 md:flex-row h-1/2">
+              <div class="p-10 md:w-1/3 bg-primaryWhite rounded-xl">
                 Pressure {{ city.main.pressure }}
               </div>
-              <div class="md:w-1/3 bg-mist rounded-xl p-10">
+              <div class="p-10 md:w-1/3 bg-primaryWhite rounded-xl">
                 Humidity {{ city.main.humidity }}
               </div>
-              <div class="md:w-1/3 bg-mist rounded-xl p-10">
+              <div class="p-10 md:w-1/3 bg-primaryWhite rounded-xl">
                 Sea Level{{ city.main.sea_level }}, Ground Level
                 {{ city.main.grnd_level }}
               </div>
@@ -103,15 +110,15 @@ const handleClick = () => {
           </div>
         </div>
       </div>
-      <!-- <div class="h-screen relative overflow-hidden"> -->
+      <!-- <div class="relative h-screen overflow-hidden"> -->
       <!-- <img :src="background" /> -->
-      <!-- <div class="absolute w-full h-full top-0 overlay"></div> -->
-      <!-- <div class="w-4/5 h-1/2 bg-white">x</div> -->
-      <!-- <div class="absolute w-full h-full top-0 p-48">
+      <!-- <div class="absolute top-0 w-full h-full overlay"></div> -->
+      <!-- <div class="w-4/5 bg-white h-1/2">x</div> -->
+      <!-- <div class="absolute top-0 w-full h-full p-48">
         <div class="flex justify-between">
           <div>
-            <h1 class="text-7xl text-white">{{ city.name }}</h1>
-            <p class="font-extralight text-2xl mt-2 text-white">
+            <h1 class="text-white text-7xl">{{ city.name }}</h1>
+            <p class="mt-2 text-2xl text-white font-extralight">
               Sunday Dec 4th
             </p>
             <img
@@ -120,7 +127,7 @@ const handleClick = () => {
             />
           </div>
           <div>
-            <p class="text-9xl text-white font-extralight">
+            <p class="text-white text-9xl font-extralight">
               {{ city.main.temp }}°
             </p>
             <div>{{ city.main }}</div>
@@ -134,7 +141,7 @@ const handleClick = () => {
             placeholder="Search a city"
             v-model="input"
           />
-          <button class="bg-cyan-500 w-20 text-white h-10" @click="handleClick">
+          <button class="w-20 h-10 text-white bg-cyan-500" @click="handleClick">
             Search
           </button>
         </div>
@@ -144,8 +151,16 @@ const handleClick = () => {
 </template>
 
 <style scoped>
-.overlay {
-  background-color: #aaa196;
+/* .overlay {
+  @apply bg-primaryBeige;
+} */
+
+.placeholder {
+  input {
+    placeholder {
+      @apply text-white;
+    }
+  }
 }
 
 .something {
